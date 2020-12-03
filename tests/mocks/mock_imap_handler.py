9e81e4701742d558b9e9b3686d95c5436fa7f5c3
Ollie Bakhtiari <olliebakhtiari@gmail.com>
2020-12-03 14:11:20 +0000

    implement required methods for imap handler mock

--- tests/mocks/mock_imap_handler.py
+++ tests/mocks/mock_imap_handler.py
@@ -1,15 +1,50 @@
+# Python standard.
+import re
+from typing import Tuple, List, Union
+
+# Local.
+from tests.mocks.mock_message import MockMessage
+
+
 class MockImapHandler:
     def __init__(self):
-        self.mailbox_map = {'Inbox': [], 'Important': []}
+        self.mailbox_map = {
+            'Inbox': {
+                'example_2@hostname.com': [1, 2, 3],
+                'example_3@hostname.de': [4, 5, 6],
+            },
+            'Important': {
+                'example_2@hostname.com': [7, 8, 9],
+                'example_3@hostname.de': [10, 11, 12],
+            },
+        }
+        self.available_mailboxes = list(self.mailbox_map.keys())
+        self.mailbox_selected = self.available_mailboxes[0]
 
-    def fetch(self):
+    @classmethod
+    def parse_header_from(cls, query: str) -> str:
+        return re.match(r"^.*\"(.*)\".*$", query).group(1)
+
+    def parse_filter(self, query: str) -> str:
+        if 'HEADER FROM' in query:
+            return self.parse_header_from(query)
+        else:
+            raise NotImplementedError('Filter parser for query not implemented.')
+
+    @classmethod
+    def fetch(cls, *args):
+        return MockMessage()
+
+    def search(self, charset, *criteria) -> Union[Tuple[str, List[int]], None]:
+        for arg in criteria:
+            valid_sender = self.parse_filter(arg)
+            if valid_sender:
+                return "OK", self.mailbox_map[self.mailbox_selected][valid_sender]
+
+    def store(self, *args):
         pass
 
-    def search(self):
-        pass
-
-    def store(self):
-        pass
-
-    def select(self):
-        pass
+    def select(self, mailbox_name: str, **kwargs):
+        if mailbox_name not in self.available_mailboxes:
+            raise ValueError('Mailbox specified not valid.')
+        self.mailbox_selected = mailbox_name
--- tests/mocks/mock_mail_account_connection.py
+++ tests/mocks/mock_mail_account_connection.py
@@ -6,7 +6,7 @@ from tests.mocks.mock_imap_handler import MockImapHandler
 class MockMailAccountConnection(MailAccountConnection):
     TRUSTED_FROM_ADDRESSES = ['example_2@hostname.com', 'example_3@hostname.de']
 
-    def __init___(self):
+    def __init__(self):
         self.connection = MockImapHandler()
         self.connection.login = ''
 
