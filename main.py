ff9833a06427c18d63617a751f2f4038983c05aa
Ollie Bakhtiari <olliebakhtiari@gmail.com>
2020-12-03 13:31:32 +0000

    add logging

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # Local.
 from settings import USERNAME, PASSWORD, MAIL_SERVER
 from src.mail_account_connection import MailAccountConnection
+from tools.logger import *
 
 
 def download_latest_mail_attachments():
     mail_account_connection = MailAccountConnection(MAIL_SERVER, USERNAME, PASSWORD)
-    emails = mail_account_connection.get_unread_mail()
+    logger.debug('Checking for mail...')
+    emails = mail_account_connection.get_unread_mail('Inbox')
+    logger.debug('Checking for attachments and downloading...')
     for mail in emails:
         mail_account_connection.download_attachment(mail)
+    logger.debug('Complete.')
 
 
 if __name__ == '__main__':
