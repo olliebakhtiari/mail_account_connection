07f0fdf736e38ce3072a61af5ebf83e39f90d3de
Ollie Bakhtiari <olliebakhtiari@gmail.com>
2020-12-03 13:25:40 +0000

    create mock mail account connection to change the __init__ to use a mocked imap handler

---
+++ tests/mocks/mock_mail_account_connection.py
@@ -0,0 +1,15 @@
+# Local.
+from src.mail_account_connection import MailAccountConnection
+from tests.mocks.mock_imap_handler import MockImapHandler
+
+
+class MockMailAccountConnection(MailAccountConnection):
+    TRUSTED_FROM_ADDRESSES = ['example_2@hostname.com', 'example_3@hostname.de']
+
+    def __init___(self):
+        self.connection = MockImapHandler()
+        self.connection.login = ''
+
+    def _close_connection(self):
+        return 'Connection closed.'
+
